ac974df33d7ae2683533c168a5ded58ec3dd32a0
hii-sayan <67260880+hii-sayan@users.noreply.github.com>
2025-11-27 02:27:26 +0530

    fixed design

--- app/auth.tsx
+++ app/auth.tsx
@@ -3,26 +3,26 @@ import { useRouter } from "expo-router";
 import { useState } from "react";
 import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
 import { Button, Text, TextInput, useTheme } from "react-native-paper";
-export default function AuthScreen() {
 
+export default function AuthScreen() {
   const [isSignUp, setIsSignUp] = useState<boolean>(false);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [error, setError] = useState<string | null>("");
 
-  const theme = useTheme()
-  const { signIn, signUp } = useAuth();
-
+  const theme = useTheme();
   const router = useRouter();
+
+  const { signIn, signUp } = useAuth();
 
   const handleAuth = async () => {
     if (!email || !password) {
-      setError("Please fill all the fields!");
+      setError("Please fill in all fields.");
       return;
     }
 
     if (password.length < 6) {
-      setError("Password must be 6 character long!");
+      setError("Passwords must be at least 6 characters long.");
       return;
     }
 
@@ -40,6 +40,7 @@ export default function AuthScreen() {
         setError(error);
         return;
       }
+
       router.replace("/");
     }
   };
@@ -53,7 +54,7 @@ export default function AuthScreen() {
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={styles.container}
     >
-      <View style={styles.content} >
+      <View style={styles.content}>
         <Text style={styles.title} variant="headlineMedium">
           {" "}
           {isSignUp ? "Create Account" : "Welcome Back"}
@@ -61,28 +62,41 @@ export default function AuthScreen() {
 
         <TextInput
           label="Email"
+          value={email}
           autoCapitalize="none"
           keyboardType="email-address"
           placeholder="example@gmail.com"
           mode="outlined"
+          textColor="#22223b"
           style={styles.input}
           onChangeText={setEmail}
         />
 
         <TextInput
           label="Password"
+          value={password}
           autoCapitalize="none"
-          secureTextEntry
           mode="outlined"
+          secureTextEntry
+          textColor="#22223b"
           style={styles.input}
           onChangeText={setPassword}
         />
-        {error && <Text style={{ color: theme.colors.error }}> {error} </Text>}
 
-        <Button mode="contained" style={styles.button} onPress={handleAuth}>{isSignUp ? "Sign Up" : "Sign In"} </Button>
+        {error && <Text style={{ color: theme.colors.error }}> {error}</Text>}
 
-        <Button mode="text" onPress={handleSwitchMode} style={styles.switchModeButton}>
-          {isSignUp ? "Alredy have an account? Sign In" : "Don't have an Account? Sign Up"}
+        <Button mode="contained" style={styles.button} onPress={handleAuth}>
+          {isSignUp ? "Sign Up" : "Sign In"}
+        </Button>
+
+        <Button
+          mode="text"
+          onPress={handleSwitchMode}
+          style={styles.switchModeButton}
+        >
+          {isSignUp
+            ? "Already have an account? Sign In"
+            : "Don't have an account? Sign Up"}
         </Button>
       </View>
     </KeyboardAvoidingView>
@@ -92,19 +106,21 @@ export default function AuthScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#f5f5f5"
+    backgroundColor: "#f5f5f5",
   },
   content: {
     flex: 1,
     padding: 16,
-    justifyContent: "center"
+    justifyContent: "center",
   },
   title: {
     textAlign: "center",
     marginBottom: 24,
+    color: "#22223b",
   },
   input: {
     marginBottom: 16,
+    backgroundColor: "#fff",
   },
   button: {
     marginTop: 8,
